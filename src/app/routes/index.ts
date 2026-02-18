e4d01d2e0b73a8efb2a13ff7af5d5a931924d003
Israt-Nisha <isratnisha1999@gmail.com>
2026-02-18 10:18:03 +0600

    doctor created router done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { SpecialtyRoutes } from "../module/specialty/specialty.router";
 import { AuthRoutes } from "../module/auth/auth.router";
+import { UserRoutes } from "../module/user/user.router";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRoutes)
+router.use("/users", UserRoutes)
 
 
 export const IndexRoutes = router;
