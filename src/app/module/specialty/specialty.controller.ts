8eee00f706176ad2b78e579c69a89cc61117c3be
Israt-Nisha <isratnisha1999@gmail.com>
2026-02-22 06:57:55 +0600

    from-data file upload using cloudinary

--- src/app/module/specialty/specialty.controller.ts
+++ src/app/module/specialty/specialty.controller.ts
@@ -5,7 +5,10 @@ import { sendResponse } from "../../sheard/sendResponse";
 
 const createSpecialty = catchAsync(
     async (req: Request, res: Response) => {
-        const payload = req.body;
+         const payload = {
+            ...req.body,
+            icon : req.file?.path
+        };
         const result = await SpecialtyService.createSpecialty(payload);
         sendResponse(res, {
             httpStatusCode: 201,
