268fa689f2030c954f242850fbc5315e6120aa94
Israt-Nisha <isratnisha1999@gmail.com>
2026-02-24 10:47:33 +0600

    appointment book with payment

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import express, { Application, Request, Response } from "express";
 import { IndexRoutes } from "./app/routes";
 import { notFound } from "./app/middleware/notFound";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import cookieParser from "cookie-parser";
+import cron from "node-cron";
 import qs from "qs";
 import cors from "cors";
 import path from "path";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./app/lib/auth";
 import { envVars } from "./app/config/env";
+import { AppointmentService } from "./app/module/appointment/appointment.service";
+import { PaymentController } from "./app/module/payment/payment.controller";
 
 
 const app: Application = express();
@@ -17,10 +21,7 @@ app.set("query parser", (str : string) => qs.parse(str));
 app.set("view engine", "ejs");
 app.set("views",path.resolve(process.cwd(), `src/app/templates`) );
 
-app.post("/webhook", express.raw({ type: "application/json" }), async (req: Request, res: Response) => {
-    console.log("Webhook Recived:", req.body);
-    res.status(200).json({ received: true});
-})
+app.post("/webhook", express.raw({ type: "application/json" }),PaymentController.handleStripeWebhookEvent)
 
 app.use(cors({
     origin : [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
@@ -38,7 +39,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser())
 
-
+cron.schedule("*/25 * * * *", async () => {
+    try {
+        console.log("Running cron job to cancel unpaid appointments...");
+        await AppointmentService.cancelUnpaidAppointments();
+    } catch (error : any) {
+        console.error("Error occurred while canceling unpaid appointments:", error.message);    
+    }
+})
 
 app.use("/api/v1", IndexRoutes);
 
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,10 @@ import { SpecialtyRoutes } from "../module/specialty/specialty.router";
 import { AuthRoutes } from "../module/auth/auth.router";
 import { UserRoutes } from "../module/user/user.router";
 import { DoctorRoutes } from "../module/doctor/doctor.router";
+import { scheduleRoutes } from "../module/schedule/schedule.router";
+import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.router";
+import { AppointmentRoutes } from "../module/appointment/appointment.router";
+import { AdminRoutes } from "../module/admin/admin.router";
 
 const router = Router();
 
@@ -10,6 +14,10 @@ router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRoutes)
 router.use("/users", UserRoutes)
 router.use("/doctors", DoctorRoutes)
+router.use("/admins", AdminRoutes)
+router.use("/schedules", scheduleRoutes)
+router.use("/doctor-schedules", DoctorScheduleRoutes)
+router.use("/appointments", AppointmentRoutes)
 
 
 export const IndexRoutes = router;
